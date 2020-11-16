03c07f13890c875885110af520f4869217e4c270
Seonmi-Hwang <hsm3530@gmail.com>
2020-11-16 16:11:47 +0900

    bulid: Add configuration files to apply validation.properties by sm

--- goodsom/src/main/java/com/example/goodsom/GoodsomApplication.java
+++ goodsom/src/main/java/com/example/goodsom/GoodsomApplication.java
@@ -2,6 +2,8 @@ package com.example.goodsom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,8 +13,13 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @EnableScheduling
-public class GoodsomApplication {
+public class GoodsomApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GoodsomApplication.class);
+	} // validation.properties 파일을 위해 추가
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GoodsomApplication.class, args);
 		System.out.println("Web Server START!");
---
+++ goodsom/src/main/java/com/example/goodsom/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.example.goodsom;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:/messages/validation");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+}
