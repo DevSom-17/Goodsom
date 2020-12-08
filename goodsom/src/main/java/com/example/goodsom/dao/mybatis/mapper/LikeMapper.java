94f3accb8902b8f83bf5205d3ad54f9b4723fd93
YeJin Lee <bambi_21@daum.net>
2020-12-08 23:59:41 +0900

    feat: Create LikeMapper by YJ

---
+++ goodsom/src/main/java/com/example/goodsom/dao/mybatis/mapper/LikeMapper.java
@@ -0,0 +1,18 @@
+package com.example.goodsom.dao.mybatis.mapper;
+
+import java.util.List;
+
+public interface LikeMapper {
+
+	void likeAuction(int userId, int auctionId);
+	
+	void likeGroupBuy(int userId, int groupBuyId);
+
+	int unlikeAuction(int userId, int auctionId);
+	
+	int unlikeGroupBuy(int userId, int groupBuyId);
+	
+	List<Integer> getAuctionOrderList(int userId);
+	
+	List<Integer> getLikeGroupBuyListByUserId(int userId);
+}
