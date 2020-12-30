8f4919f101b5660f3e1ed8c75a02f8e80a40188d
YeJin Lee <bambi_21@daum.net>
2020-12-31 02:24:24 +0900

    feat: Develop error catching feature in mobile certification by YJ

--- goodsom/src/main/java/com/example/goodsom/controller/user/SmsController.java
+++ goodsom/src/main/java/com/example/goodsom/controller/user/SmsController.java
@@ -1,6 +1,7 @@
 package com.example.goodsom.controller.user;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import org.json.simple.JSONObject;
@@ -16,7 +17,7 @@ public class SmsController {
 	
 	@GetMapping("/sendSMS.do")
 	@ResponseBody
-	public String sendSMS(String phoneNumber) {
+	public Map<String, Object> sendSMS(String phoneNumber) {
 
 //		각자의 API_key와 api_secret 적는 부분
 		String api_key = "";						// 각자의 API_key 적기
@@ -40,24 +41,33 @@ public class SmsController {
         // 4 params(to, from, type, text) are mandatory. must be filled
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("to", phoneNum);    // 수신전화번호
-        params.put("from", "01072264789");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+        params.put("from", "010");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
         params.put("type", "SMS");
         params.put("text", "[Goodsom]인증번호는 " + "["+numStr+"]" + "입니다.");
         params.put("app_version", "test app 1.2"); // application name and version
 
-//        sms기능을 사용하기 위해서는 이 부분을 주석해체하면 됩니다.
-//        Message coolsms = new Message(api_key, api_secret);
+//      jsp에 넘겨줄 값들
+        Map<String, Object> map = new HashMap<String, Object>();
+        
+//      sms기능을 사용하기 위해서는 이 부분을 주석해제하면 됩니다.
+        Message coolsms = new Message(api_key, api_secret);
 //        try {
 //            JSONObject obj = (JSONObject) coolsms.send(params);
 //            System.out.println(obj.toString());
+//            System.out.println(obj.get("error_count"));
+//            if (((Long)obj.get("error_count")).intValue() > 0) {
+//            	map.put("errorOccured", "true");
+//            } else {
+//            	map.put("errorOccured", "false");
+//            }
 //        } catch (CoolsmsException e) {
-//            System.out.println(e.getMessage());
-//            System.out.println(e.getCode());
+//            System.out.println(e.getMessage());		// REST API 에러코드
+//            System.out.println(e.getCode());		// 에러메시지
 //        }
-
         System.out.println("수신자 번호 : " + phoneNumber);
         System.out.println("인증번호 : " + numStr);
-        return numStr;
+        map.put("certNum", numStr);
+        return map;
         
     }
 }
